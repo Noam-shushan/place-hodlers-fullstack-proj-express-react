5162e7d4ab3dfbd24e6f7dba72627d9ddd348f0e
Noam shushan <noam8shu@gmail.com>
2023-06-13 18:27:42 +0300

    add cors to connect the server to the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
 export default function App() {
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/users`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
+
   return (
     <>
       <h1 className="text-5xl text-center">
--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ dotenv.config()
 
 import express from 'express'
 import { router as usersRouter } from './routes/users.js'
-
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api/users', usersRouter)
 
